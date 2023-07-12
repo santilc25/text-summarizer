6a9bc45982d189ddad26609cef98b457109e557e
Santiago <santiagolopezcarmona.ds@gmail.com>
2023-07-12 10:22:26 -0500

    Data ingestion pipeline created

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
 from src.logging import logger
+from src.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 
-
-logger.info("Welcome to log")
+STAGE_NAME = "Data Ingestion stage"
+try:
+    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
+    data_ingestion = DataIngestionTrainingPipeline()
+    data_ingestion.main()
+    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)    
