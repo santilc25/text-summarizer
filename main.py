49f43439a388afd1c102a996a7aec23a122f9c3b
Santiago Lopez <santiagolopezcarmona.ds@gmail.com>
2023-07-26 09:43:29 -0500

    Data transformation completed

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from src.logging import logger
 from src.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 from src.pipeline.stage_02_data_validation import DataValidationTrainingPipeline
+from src.pipeline.stage_03_data_transformation import DataTransformationTrainingPipeline
+
 
 STAGE_NAME = "Data Ingestion stage"
 try:
@@ -11,12 +13,21 @@ try:
 except Exception as e:
         logger.exception(e)    
         
-
 STAGE_NAME = "Data Validation stage"
 try:
    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
    data_validation = DataValidationTrainingPipeline()
    data_validation.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
+
+STAGE_NAME = "Data Transformation stage"
+try:
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
+   data_tranformation = DataTransformationTrainingPipeline()
+   data_tranformation.main()
    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
 except Exception as e:
         logger.exception(e)
